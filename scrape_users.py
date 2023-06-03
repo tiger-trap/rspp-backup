63351fde720d1b056c4553c0d4b2bdff44e2cab0
skunz42 <skunz1@binghamton.edu>
2023-06-03 21:28:58 +0000

    added token fetching

--- scrape_users.py
+++ scrape_users.py
@@ -8,7 +8,6 @@ LIMIT = 100
 RECURSE = False
 
 def get_env_vars():
-    print(os.environ['DB_PASSWORD'])
     return {'client_id': os.environ['CLIENT_ID'],
             'secret_id': os.environ['SECRET_ID'],
             'reddit_user': os.environ['REDDIT_USERNAME'],
@@ -32,11 +31,34 @@ def disconnect_from_db(conn):
     """ """
     conn.close()
 
+def get_token(env_vars):
+    """ """
+    auth = requests.auth.HTTPBasicAuth(env_vars['client_id'], env_vars['secret_id'])
+
+    data = {'grant_type': 'password',
+            'username': env_vars['reddit_user'],
+            'password': env_vars['reddit_pass']}
+
+    headers = {'User-Agent': 'rspp/0.1.1'}
+
+    res = requests.post('https://www.reddit.com/api/v1/access_token',
+            auth=auth, data=data, headers=headers)
+
+    token = res.json()['access_token']
+    headers = {**headers, **{'Authorization': f"bearer {token}"}}
+
+    return headers
+
 def main():
     """ """
     env_vars = get_env_vars()
+    print("Fetched env variables")
     conn = connect_to_db(env_vars)
-    print(conn)
+    print("Connected to database")
+    token = get_token(env_vars)
+    print("Fetched token")
+    disconnect_from_db(conn)
+    print("Disconnected from database")
 
 if __name__ == "__main__":
     main()
